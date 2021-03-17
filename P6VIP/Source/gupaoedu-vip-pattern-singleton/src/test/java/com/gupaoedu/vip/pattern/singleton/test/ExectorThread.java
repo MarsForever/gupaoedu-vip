2272a7b78e49ffa8c9e4c5d9b75fd29b6c7cf00d
MarsForever <wuguijiadeyu@gmail.com>
2021-03-17 23:31:30 +0900

    add pdf and java

--- P6VIP/Source/gupaoedu-vip-pattern-singleton/src/test/java/com/gupaoedu/vip/pattern/singleton/test/ExectorThread.java
+++ P6VIP/Source/gupaoedu-vip-pattern-singleton/src/test/java/com/gupaoedu/vip/pattern/singleton/test/ExectorThread.java
@@ -12,9 +12,9 @@ public class ExectorThread implements Runnable{
     public void run() {
 
 //        LazySimpleSingletion instance = LazySimpleSingletion.getInstance();
-//        LazyDoubleCheckSingleton instance = LazyDoubleCheckSingleton.getInstance();
-        ThreadLocalSingleton instance = ThreadLocalSingleton.getInstance();
-        System.out.println(ThreadLocalSingleton.getInstance());
+        LazyDoubleCheckSingleton instance = LazyDoubleCheckSingleton.getInstance();
+//        ThreadLocalSingleton instance = ThreadLocalSingleton.getInstance();
+//        System.out.println(ThreadLocalSingleton.getInstance());
         System.out.println(Thread.currentThread().getName() + ":" + instance);
 
     }
